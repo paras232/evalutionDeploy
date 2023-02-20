c8df7a4136e8025f8c2a47a4f40101cc42b977a7
paras232 <parasmanipatnayak@gmail.com>
2023-02-20 15:38:02 +0530

    2st commit

--- unit-4/sprint-4/evaluation/backend/index.js
+++ unit-4/sprint-4/evaluation/backend/index.js
@@ -1,6 +1,7 @@
 const express=require("express")
+const {connection}=require("./db")
 
-
+require("dotenv").config()
 const app=express()
 
 app.use(express.json())
@@ -9,6 +10,12 @@ app.get("/",(req,res)=>{
     "Home Page"
 })
 
-app.listen(8080,()=>{
-    console.log(` server is running on port 8080`)
+app.listen(process.env.port,async()=>{
+    try{
+        await connection
+        console.log("connected to db")
+    }catch(err){
+        console.log({"error":err.message})
+    }
+    console.log(` server is running on port ${process.env.port}`)
 })
