630ee14e9c58a776fe5125a4869ebae1991b4f35
paras232 <parasmanipatnayak@gmail.com>
2023-03-03 17:41:29 +0530

    assignment 2 day3 compelete

---
+++ NXM-111/sprint-1/day-3/assignments/masai-cp-problems-1019-1586-WQAYXZ/index.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+
+const input=require("process").argv.slice(2)
+// console.log(input)
+const operation=input[0]
+const operant1=input[1]
+const operant2=input[2]
+const operant3=input[3]
+
+//create a file
+if(operation =="create"){
+    fs.writeFile(`${operant1}`,`${operant2}\n`,(err)=>{
+        if(err){
+            console.log("can not create a file right now")
+        }else {
+            console.log("new file is created in explorer")
+        }
+    })
+}
+
+//rename a file
+if(operation=="rename"){
+    fs.rename(`${operant1}`,`${operant2}`,(err)=>{
+        if(err){
+            console.log("rename failed")
+        }else {
+            console.log("file is renamed")
+        }
+    })
+}
+
+//append a file
+if(operation=="append"){
+    fs.appendFile(`${operant1}`,`\n${operant2}`,(err)=>{
+        if(err){
+            console.log("append failed")
+        }else {
+            console.log("new code appended")
+        }
+    })
+}
+
+//read a file
+if(operation=="read"){
+    fs.readFile(`${operant1}`,"utf8",(err,data)=>{
+        if (err) throw err;
+        console.log(data)
+    })
+}
+
+//list a file
+if(operation=="list"){
+    fs.readdir(`./${operant1}`,(err,file)=>{
+        if(err){
+            console.log("failed")
+        }else {
+            console.log(file)
+        }
+    })
+}
+
+
+
+//delete a file
+if(operation=="delete"){
+    fs.unlink(`${operant1}`,(err)=>{
+        if(err){
+            console.log("deletion failed");
+        }else{
+            console.log("file deleted successfully");
+        }
+    })
+}
